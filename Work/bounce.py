f14629d29abb09a413642c2d456ed277cff4140a
Luke Hoffmann <992315+lukehoffmann@users.noreply.github.com>
2024-09-13 10:59:46 +0930

    1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+
+bounce = 0
+height = 60.0
+
+while (bounce:= bounce + 1) <= 10:
+    print(bounce, round(height, 4))
+    height *= 3 / 5
--- Work/sears.py
+++ Work/sears.py
@@ -1,9 +1,10 @@
 bill_thickness = 0.11 * 0.001 # Meters (0.11 mm)
-sears_height = 442 # Meters
+# target_height = int(input('How high?')) # 442 # Meters
+target_height = 442 # Meters
 num_bills = 1
 day = 1
 
-while (stack_height := num_bills * bill_thickness) < sears_height:
+while (stack_height := num_bills * bill_thickness) < target_height:
     print(day, num_bills, stack_height)
     day += 1
     num_bills *= 2
