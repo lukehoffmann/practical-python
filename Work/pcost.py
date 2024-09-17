dbee591e4b89e7539ad2ae0f09e1fead4a2421a9
Luke Hoffmann <992315+lukehoffmann@users.noreply.github.com>
2024-09-17 11:09:54 +0930

    Portfolio cost, with function

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,19 @@
 # pcost.py
 #
 # Exercise 1.27
+
+def portfolio_cost(filename):
+    cost = 0.0
+    with open(filename, 'rt') as f:
+        next(f) # header
+        for line in f:
+            _, shares, price = line.split(',')
+            try:
+                cost += int(shares) * float(price)
+            except ValueError as e:
+                print('Warning, invalid values in', line, e)
+    return cost
+
+
+cost = portfolio_cost('Data/portfolio.csv')
+print(f"Total cost ${cost:.2f}")
