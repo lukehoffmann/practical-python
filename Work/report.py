1e6ae68aab9bc5fabb0d7811aed7fe1cd45d5b46
Luke Hoffmann <992315+lukehoffmann@users.noreply.github.com>
2024-09-17 15:51:33 +0930

    reformat source

--- Work/report.py
+++ Work/report.py
@@ -35,20 +35,23 @@ def make_report(portfolio, prices):
         stock["new_value"] = stock["shares"] * stock["new_price"]
         stock["price_change"] = stock["new_price"] - stock["price"]
         stock["change"] = stock["new_value"] - stock["old_value"]
-        report.append((stock['name'], stock['shares'], stock['new_price'], stock['price_change']))
+        report.append(
+            (stock["name"], stock["shares"], stock["new_price"], stock["price_change"])
+        )
     return report
+
 
 prices = read_prices("Data/prices.csv")
 portfolio = read_portfolio("Data/portfolio.csv")
 report = make_report(portfolio, prices)
 
 headers = ["Name", "Shares", "Price", "Change"]
-print(''.join([f'{header:>10s}' for header in headers]))
-print(''.join([' ' + '-' * 9 for _ in headers]))
+print("".join([f"{header:>10s}" for header in headers]))
+print("".join([" " + "-" * 9 for _ in headers]))
 for name, shares, price, change in report:
     price = f"${price:>.2f}"
     symbol = ("⬆" if change > 0.0
-        else "⬇" if change < 0.0
-        else "-")
+         else "⬇" if change < 0.0
+         else "-")
     change = f"{symbol} ${abs(change):.2f}"
-    print(f'{name:>10s}{shares:>10d}{price:>10s}{change:>10s}')
+    print(f"{name:>10s}{shares:>10d}{price:>10s}{change:>10s}")
