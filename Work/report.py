d29e6745a5c23d3ddd0484a0fa8a7d346b3461cb
Luke Hoffmann <992315+lukehoffmann@users.noreply.github.com>
2024-09-17 12:44:30 +0930

    simple report using tuple

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,20 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+def read_portfolio(filename):
+    '''Read a portfolio file into a list of tuples of form (name, shares, price)'''
+    portfolio = []
+    with open(filename, 'rt') as f:
+        next(f) # header
+        rows = csv.reader(f)
+        for row in rows:
+            portfolio.append((row[0], int(row[1]), float(row[2])))
+    return portfolio
+
+portfolio = read_portfolio('Data/portfolio.csv')
+
+total = sum([shares * price for _, shares, price in portfolio])
+print(total)
