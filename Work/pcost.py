bd56687e79525a71b1dd6217aff1b2172fdaebe0
Luke Hoffmann <992315+lukehoffmann@users.noreply.github.com>
2024-09-17 11:12:08 +0930

    Use csv module

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,16 +2,19 @@
 #
 # Exercise 1.27
 
+import csv
+
 def portfolio_cost(filename):
     cost = 0.0
     with open(filename, 'rt') as f:
-        next(f) # header
-        for line in f:
-            _, shares, price = line.split(',')
+        rows = csv.reader(f)
+        next(rows) # header
+        for row in rows:
+            _, shares, price = row
             try:
                 cost += int(shares) * float(price)
             except ValueError as e:
-                print('Warning, invalid values in', line, e)
+                print('Warning, invalid values in', row, e)
     return cost
 
 
