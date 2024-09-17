cb1a6c2c3da77a77b49b704ae1e37a8db6ec0333
Luke Hoffmann <992315+lukehoffmann@users.noreply.github.com>
2024-09-17 11:14:24 +0930

    Use file argument from commandline

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,6 +3,7 @@
 # Exercise 1.27
 
 import csv
+import sys
 
 def portfolio_cost(filename):
     cost = 0.0
@@ -17,6 +18,10 @@ def portfolio_cost(filename):
                 print('Warning, invalid values in', row, e)
     return cost
 
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
 
-cost = portfolio_cost('Data/portfolio.csv')
+cost = portfolio_cost(filename)
 print(f"Total cost ${cost:.2f}")
