140b0a4c0cb8e995233d625f47dfee2d559b45fc
Luke Hoffmann <992315+lukehoffmann@users.noreply.github.com>
2024-09-17 15:24:14 +0930

    Better formatting

--- Work/report.py
+++ Work/report.py
@@ -3,7 +3,6 @@
 # Exercise 2.4
 
 import csv
-from pprint import pprint
 
 
 def read_prices(filename):
@@ -35,34 +34,19 @@ def format_profit(change):
     symbol = ("⬆" if change > 0.0
         else "⬇" if change < 0.0
         else "-")
-    return f"({symbol}{ds.format(abs(change))})"
+    return f"({symbol}{abs(change):.2f})"
 
 
 prices = read_prices("Data/prices.csv")
-pprint(prices)
-
 portfolio = read_portfolio("Data/portfolio.csv")
-pprint(portfolio)
 
 for stock in portfolio:
     stock["old_value"] = stock["shares"] * stock["price"]
     stock["new_value"] = stock["shares"] * prices[stock["name"]]
     stock["change"] = stock["new_value"] - stock["old_value"]
-    print(
-        stock["name"],
-        ds.format(stock["old_value"]),
-        "→",
-        ds.format(stock["new_value"]),
-        format_profit(stock["change"]),
-    )
+    print(f'{stock["name"]:<7s}{stock["old_value"]:10.2f} →{stock["new_value"]:10.2f}{format_profit(stock["change"]):>15s}')
 
 portfolio_total = sum([stock["old_value"] for stock in portfolio])
 new_total = sum([stock["new_value"] for stock in portfolio])
 profit = new_total - portfolio_total
-print(
-    "total",
-    ds.format(portfolio_total),
-    "→",
-    ds.format(new_total),
-    format_profit(profit),
-)
+print(f'{"total":<7s}{portfolio_total:10.2f} →{new_total:10.2f}{format_profit(profit):>15s}')
