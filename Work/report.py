046be645582d16ef0560f4c55be383537cac7454
Luke Hoffmann <992315+lukehoffmann@users.noreply.github.com>
2024-09-19 16:55:43 +0930

    Organisation

--- Work/report.py
+++ Work/report.py
@@ -6,6 +6,12 @@ from collections import Counter
 import csv
 
 
+def print_portfolio_report(portfolio_filename, prices_filename):
+    prices = read_prices(prices_filename)
+    portfolio = read_portfolio(portfolio_filename)
+    print_report(portfolio, prices)
+
+
 def read_prices(filename):
     """Read a pricelist from a csv"""
     with open(filename, "rt") as f:
@@ -16,53 +22,49 @@ def read_prices(filename):
 def read_portfolio(filename):
     '''Read a portfolio file into a list of dicts with keys "name", "shares", "price"'''
     column_defs = [
-        {"name": "name", "type": str},
-        {"name": "shares", "type": int},
-        {"name": "price", "type": float},
+        {"header": "name", "type": str},
+        {"header": "shares", "type": int},
+        {"header": "price", "type": float},
     ]
     with open(filename, "rt") as f:
         rows = csv.reader(f)
         headers = next(rows)
         for column in column_defs:
-            column["index"] = headers.index(column["name"])
+            column["index"] = headers.index(column["header"])
 
-        portfolio = [
+        return [
             {
-                column["name"]: column["type"](row[column["index"]])
+                column["header"]: column["type"](row[column["index"]])
                 for column in column_defs
             }
             for row in rows
         ]
 
+
+def print_report(portfolio, prices):
+    headers = ["Name", "Shares", "Was", "Now", "Position"]
+    print(" ".join(["%10s" % header for header in headers]))
+    print(" ".join([10 * "-" for _ in headers]))
     for stock in portfolio:
-        stock["shares"] = int(stock["shares"])
-        stock["price"] = float(stock["price"])
-    return portfolio
+        old_price = stock["price"]
+        new_price = prices[stock["name"]]
+        stock["cost"] = stock["shares"] * old_price
+        stock["value"] = stock["shares"] * new_price
+
+        was = f"${old_price:>.2f}"
+        now = f"${new_price:>.2f}"
+
+        position = stock["value"] - stock["cost"]
+        symbol = ("⬆" if position > 0.0
+            else "⬇" if position < 0.0
+            else "-")
+        position_dollars = f"${abs(position):.2f}"
+        print(
+            f"{stock['name']:>10s} {stock['shares']:>10d} {was:>10s} {now:>10s} {position_dollars:>10s} {symbol}"
+        )
+
+    total = sum([stock["value"] for stock in portfolio])
+    print("Total value", total)
 
 
-prices = read_prices("Data/prices.csv")
-portfolio = read_portfolio("Data/portfolio.csv")
-
-headers = ["Name", "Shares", "Was", "Now", "Position"]
-print(" ".join(["%10s" % header for header in headers]))
-print(" ".join([10 * "-" for _ in headers]))
-for stock in portfolio:
-    old_price = stock["price"]
-    new_price = prices[stock["name"]]
-    stock["cost"] = stock["shares"] * old_price
-    stock["value"] = stock["shares"] * new_price
-
-    was = f"${old_price:>.2f}"
-    now = f"${new_price:>.2f}"
-
-    position = stock["value"] - stock["cost"]
-    symbol = ("⬆" if position > 0.0
-         else "⬇" if position < 0.0
-         else "-")
-    position_dollars = f"${abs(position):.2f}"
-    print(
-        f"{stock['name']:>10s} {stock['shares']:>10d} {was:>10s} {now:>10s} {position_dollars:>10s} {symbol}"
-    )
-
-total = sum([stock["value"] for stock in portfolio])
-print("Total value", total)
+print_portfolio_report("Data/portfolio.csv", "Data/prices.csv")
