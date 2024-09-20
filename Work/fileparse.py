d7c786f270f68eb197086d0a434b59dc30cb658c
Luke Hoffmann <992315+lukehoffmann@users.noreply.github.com>
2024-09-20 11:24:24 +0930

    Read csv files without headers as tuples

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -5,31 +5,38 @@
 import csv
 
 
-def parse_csv(filename, select=None, types=None):
+def parse_csv(filename, has_headers=True, select=None, types=None):
     """Parse a CSV file into a list of records"""
     with open(filename) as f:
         rows = csv.reader(f)
 
-        # Read the file headers
-        headers = next(rows)
+        # Read the file headers from the first row
+        if has_headers:
+            headers = next(rows)
 
-        # Create a dictionary of selected values for each row
         records = []
         for row in rows:
             if not row:
                 continue
 
-            # Filter for the selected headers by their index in the top row
-            if select:
+            # Select columns by their index in the top row
+            if has_headers and select:
                 row = [ row[headers.index(column_name)] for column_name in select ]
+
             # Convert the remaining values to specified type
             if types:
                 row = [ type(value) for value, type in zip(row, types) ]
-            records.append(dict(zip(select or headers, row)))
 
-        # return [make_record(row) for row in rows if row]
+            if has_headers:
+                # Create a dictionary of selected values by name
+                records.append(dict(zip(select or headers, row)))
+            else:
+                # Create a tuple of all values
+                records.append(tuple(row))
+
     return records
 
 print(parse_csv("Data/portfolio.csv"))
 print(parse_csv("Data/portfolio.csv", select=["name", "price"]))
 print(parse_csv("Data/portfolio.csv", types=[str, int, float]))
+print(parse_csv("Data/prices.csv", has_headers=False, types=[str, float]))
