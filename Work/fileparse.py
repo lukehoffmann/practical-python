ab1ec5b8cb264dd1393a5ad5fca419f27bedc31e
Luke Hoffmann <992315+lukehoffmann@users.noreply.github.com>
2024-09-20 10:57:16 +0930

    Add optional column selection

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,19 +4,24 @@
 
 import csv
 
-def parse_csv(filename):
-    '''Parse a CSV file into a list of records'''
+
+def parse_csv(filename, select=None):
+    """Parse a CSV file into a list of records"""
     with open(filename) as f:
         rows = csv.reader(f)
 
         # Read the file headers
         headers = next(rows)
-        # records = []
-        # for row in rows:
-        #     if not row:
-        #         continue
-        #     record = dict(zip(headers, row))
-        #     records.append(record)
+        # Find the index of selected headers, or use all headers
+        selected_headers = {
+            header: headers.index(header) for header in select or headers
+        }
+        # Create a dictionary of selected values for each row
+        make_record = lambda row: {
+            header: row[i] for header, i in selected_headers.items()
+        }
+        return [make_record(row) for row in rows if row]
 
-    # return records
-        return [dict(zip(headers, row)) for row in rows if row]
+
+print(parse_csv("Data/portfolio.csv"))
+print(parse_csv("Data/portfolio.csv", select=["name", "price"]))
