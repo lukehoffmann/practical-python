811b846d3c96dc3144e3ddf8ca5f0caf10902aed
Luke Hoffmann <992315+lukehoffmann@users.noreply.github.com>
2024-09-20 11:18:36 +0930

    Add optional type conversion

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -5,24 +5,31 @@
 import csv
 
 
-def parse_csv(filename, select=None):
+def parse_csv(filename, select=None, types=None):
     """Parse a CSV file into a list of records"""
     with open(filename) as f:
         rows = csv.reader(f)
 
         # Read the file headers
         headers = next(rows)
-        # Find the index of selected headers, or use all headers
-        select = select or headers
-        selected_columns = {
-            column_name: headers.index(column_name) for column_name in select
-        }
-        # Create a dictionary of selected values for each row
-        make_record = lambda row: {
-            column_name: row[index] for column_name, index in selected_columns.items()
-        }
-        return [make_record(row) for row in rows if row]
 
+        # Create a dictionary of selected values for each row
+        records = []
+        for row in rows:
+            if not row:
+                continue
+
+            # Filter for the selected headers by their index in the top row
+            if select:
+                row = [ row[headers.index(column_name)] for column_name in select ]
+            # Convert the remaining values to specified type
+            if types:
+                row = [ type(value) for value, type in zip(row, types) ]
+            records.append(dict(zip(select or headers, row)))
+
+        # return [make_record(row) for row in rows if row]
+    return records
 
 print(parse_csv("Data/portfolio.csv"))
 print(parse_csv("Data/portfolio.csv", select=["name", "price"]))
+print(parse_csv("Data/portfolio.csv", types=[str, int, float]))
