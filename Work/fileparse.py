492736eda49f7ebc7a6452c3c7bb90467142c101
Luke Hoffmann <992315+lukehoffmann@users.noreply.github.com>
2024-09-23 11:02:52 +0930

    Go back to looping over records

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -5,7 +5,7 @@
 import csv
 
 
-def parse_csv(filename, delimeter=",", has_headers=True, select=None, types=None):
+def parse_csv(filename, delimeter=',', has_headers=True, select=None, types=None):
     """Parse a CSV file into a list of records"""
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimeter)
@@ -13,35 +13,31 @@ def parse_csv(filename, delimeter=",", has_headers=True, select=None, types=None
         # Read the file headers from the first row
         if has_headers:
             headers = next(rows)
-        # Read the remaining rows
-        records = [row for row in rows if row]
 
-        # Select columns by their index in the top row
-        if has_headers and select:
-            records = [
-                [record[headers.index(column_name)] for column_name in select]
-                for record in records
-            ]
+        records = []
+        for row in rows:
+            if not row:
+                continue
 
-        # Convert the remaining values to specified type
-        if types:
-            records = [
-                [type(value) for value, type in zip(record, types)]
-                for record in records
-            ]
+            # Select columns by their index in the top row
+            if has_headers and select:
+                row = [ row[headers.index(column_name)] for column_name in select ]
 
-        if has_headers:
-            # Create a dictionary of selected values by name
-            records = [dict(zip(select or headers, record)) for record in records]
-        else:
-            # Create a tuple of all values
-            records = [tuple(record) for record in records]
+            # Convert the remaining values to specified type
+            if types:
+                row = [ type(value) for value, type in zip(row, types) ]
+
+            if has_headers:
+                # Create a dictionary of selected values by name
+                records.append(dict(zip(select or headers, row)))
+            else:
+                # Create a tuple of all values
+                records.append(tuple(row))
 
     return records
-
 
 print(parse_csv("Data/portfolio.csv"))
 print(parse_csv("Data/portfolio.csv", select=["name", "price"]))
 print(parse_csv("Data/portfolio.csv", types=[str, int, float]))
 print(parse_csv("Data/prices.csv", has_headers=False, types=[str, float]))
-print(parse_csv("Data/portfolio.dat", delimeter=" ", types=[str, int, float]))
+print(parse_csv("Data/portfolio.dat", delimeter=' ', types=[str, int, float]))
