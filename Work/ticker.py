0256fd77e0136c3894db675c85e977b7214b7f1e
Luke Hoffmann <luke.hoffmann@bom.gov.au>
2025-07-11 15:58:15 +1000

    Use formatters in ticker

--- Work/ticker.py
+++ Work/ticker.py
@@ -1,49 +1,51 @@
 from follow import follow
 import report
+import tableformat
 import csv
 
-columns = ['name', 'price', 'change']
+column_names = ['name', 'price', 'change']
+column_indices = [0, 1, 4]
+column_types = [str, float, float]
 
 def select_columns(rows, indices):
     return ([row[index] for index in indices] for row in rows)
 
+
 def convert_types(rows, types):
     return ([t(val) for val, t in zip(row, types)] for row in rows)
+
 
 def make_dicts(rows, keys):
     return (dict(zip(keys, row)) for row in rows)
 
+
 def parse_stock_data(lines):
     rows = csv.reader(lines)
-    rows = select_columns(rows, [0, 1, 4])
-    rows = convert_types(rows, [str, float, float])
-    rows = make_dicts(rows, columns)
+    rows = select_columns(rows, column_indices)
+    rows = convert_types(rows, column_types)
+    rows = make_dicts(rows, column_names)
     return rows
+
 
 def filter_symbols(rows, names):
     return (r for r in rows if r['name'] in names)
 
-def print_stock_csv(rows):
-    print(str.join(',', (h for h in columns)))
-    for row in rows:
-        print(str.join(',', (str(v) for v in row.values())))
-
-def print_stock_report(rows):
-    print(" ".join(["%10s" % h.title() for h in columns]))
-    print(" ".join([10 * "-" for _ in columns]))
-    for row in rows:
-        print(str.join(' ', (str(v).rjust(10) for v in row.values())))
 
 def ticker(portfile, logfile, format):
     portfolio = report.read_portfolio(portfile)
 
+    if format == 'csv':
+        formatter = tableformat.CsvTableFormatter()
+    else:
+        formatter = tableformat.TextTableFormatter()
+    formatter.headings(h.title() for h in column_names)
+
     rows = parse_stock_data(follow(logfile))
     rows = filter_symbols(rows, portfolio)
 
-    if format == 'csv':
-        print_stock_csv(rows)
-    else:
-        print_stock_report(rows)
+    for row in rows:
+        formatter.row(str(v) for v in row.values())
+
 
 if __name__ == '__main__':
-    ticker('Data/portfolio.csv', 'Data/stocklog.csv', 'csv')
+    ticker('Data/portfolio.csv', 'Data/stocklog.csv', 'txt')
